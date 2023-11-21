7274eec897fe5f384d3134b64d824dc89bbc02cc
snowby666 <giabao070206@gmail.com>
2023-11-21 21:28:22 +0700

    Add build

---
+++ build/lib/poe_api_wrapper/proxies.py
@@ -0,0 +1,23 @@
+try:
+    from ballyregan import ProxyFetcher
+    from ballyregan.models import Protocols, Anonymities
+    PROXY = True
+except:
+    PROXY = False
+
+if PROXY:
+    def fetch_proxy():
+        fetcher = ProxyFetcher()
+        try:
+            proxies = fetcher.get(
+            limit=10,
+            protocols=[Protocols.HTTP],
+            anonymities=[Anonymities.ANONYMOUS],
+            )
+        except:
+            print("No Anonymous proxies found. Switching to normal proxies ...") 
+            proxies = fetcher.get(
+            limit=10,
+            protocols=[Protocols.HTTP],
+            )
+        return proxies
