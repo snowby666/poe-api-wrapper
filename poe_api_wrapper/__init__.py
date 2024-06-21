6c80a3f44f46fc340232f1ce5a62b2d8f6b2e74f
snowby666 <giabao070206@gmail.com>
2024-06-21 14:13:12 +0700

    Update: Fixed 3rd party proxy client issue (Clash)

--- poe_api_wrapper/__init__.py
+++ poe_api_wrapper/__init__.py
@@ -1,3 +1,21 @@
+import os, socket
+
+def is_using_proxy(address, port):
+    try:
+        socket.create_connection((address, port), timeout=5)
+        return True
+    except Exception as e:
+        return False
+
+if is_using_proxy("127.0.0.1", "7890"):
+    print("""
+        3rd party proxy client detected. 
+        Updating environment variables ...
+        """)
+
+    os.environ["http_proxy"] = "http://127.0.0.1:7890"
+    os.environ["https_proxy"] = "http://127.0.0.1:7890"
+
 from .api import PoeApi
 from .async_api import AsyncPoeApi
 from .example import PoeExample
